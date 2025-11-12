fd7e77d3b3b4af02fe7a78ce7a25e610e05ca35c
“Andrey <andreya@fr24.com>
2025-11-12 15:53:42 +0400

    introduces Package.swift wrapping InMobiSDK version 11.1.0

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "InMobiSDK",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "InMobiSDK",
+            targets: ["InMobiSDK"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "InMobiSDK",
+            url: "https://dl.inmobi.com/inmobi-sdk/IM/InMobi-iOS-SDK-11.1.0.zip",
+            checksum: "94ee97c3fc99e0111db5dd89cc872ab52a88c52b2af96fa6cd246021a114a4ef"
+        )
+    ]
+)
